fde8a0acfd588db56119591d23308c1e8ffef27a
Antony Jaison <antonyjaison639@gmail.com>
2024-02-17 02:49:46 +0530

    added profile page

--- src/components/user_avatar.jsx
+++ src/components/user_avatar.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Location from "./location";
 import { auth } from "@/lib/auth";
-
+import Link from "next/link";
 
 const UserAvatar = async () => {
-    const { user } = await auth();
+  const { user } = await auth();
 
   return (
     <div className="flex justify-between items-center">
-      <Avatar>
-        <AvatarImage src={user?.image} />
-        <AvatarFallback>{user?.name?.slice(0,2).toUpperCase()}</AvatarFallback>
-      </Avatar>
-      <Location/>
+      <Link href="/profile">
+        <Avatar>
+          <AvatarImage src={user?.image} />
+          <AvatarFallback>
+            {user?.name?.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </Link>
+      <Location />
       <div></div>
     </div>
   );
