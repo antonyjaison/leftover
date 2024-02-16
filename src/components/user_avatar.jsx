69fe2847d0905e45dcf38637a2e4659e0d46ccd9
Antony Jaison <antonyjaison639@gmail.com>
2024-02-16 22:45:21 +0530

    added avatar dynamic

--- src/components/user_avatar.jsx
+++ src/components/user_avatar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Location from "./location";
+import { auth } from "@/lib/auth";
 
-const UserAvatar = () => {
+
+const UserAvatar = async () => {
+    const { user } = await auth();
+
   return (
     <div className="flex justify-between items-center">
       <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarImage src={user?.image} />
+        <AvatarFallback>{user?.name?.slice(0,2).toUpperCase()}</AvatarFallback>
       </Avatar>
       <Location/>
       <div></div>
