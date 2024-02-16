4b25c6f8a88eb7ba55c63870019750dc47b8239d
Antony Jaison <antonyjaison639@gmail.com>
2024-02-17 02:07:25 +0530

    added homepage drawer

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -1,14 +1,34 @@
+'use client'
+
 import React from 'react'
 
 const Location = () => {
+
+    const getCurrentLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    }
+
+    const showPosition = (position) => {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        console.log('latitude', latitude, 'longitude', longitude)
+
+        // api call to get location
+    }
+
+
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <button onClick={getCurrentLocation} className='flex flex-col justify-center items-center'>
         <p className=' text-[#8E8E8E] text-sm'>Location</p>
         <span className=' flex'>
             <img src="/svg/location.svg" alt="location" />
             <p className='text-[#1A1A1A] text-base'>Bangalore, KA</p>
         </span>
-    </div>
+    </button>
   )
 }
 
